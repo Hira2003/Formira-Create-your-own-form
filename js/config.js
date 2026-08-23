565889847ca8ce1841f050f13b74f62ffe04b6b4
Hira2003 <hirafuyu2003@gmail.com>
2026-08-23 22:48:17 +0100

    Update config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://psyokdlglfxbvycapbnr.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_33eztoQfIm3rYZRk7gtPXQ_KYkmBrhf";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
